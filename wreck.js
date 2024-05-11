3f5f91aa94d1e8d726c20202bc2b4e8726a06d53
nokarv <Karv13578@gmail.com>
2024-05-11 20:39:49 +0700

    Update wreck.js

--- wreck.js
+++ wreck.js
@@ -1,56 +1,47 @@
-const tanah = document.querySelectorAll('.tanah');
-const tikus = document.querySelectorAll('.tikus');
-const papanSkor = document.querySelector('.papan-skor');
-const pop = document.querySelector('#pop');
+const cursor = document.querySelector('.cursor')
+const holes = [...document.querySelectorAll('.hole')]
+const scoreEl = document.querySelector('.score span')
+let score = 0
 
-let tanahSebelumnya;
-let selesai;
-let skor;
+const sound = new Audio("audio_Pop.mp3")
 
-function randomTanah(tanah) {
-  const t = Math.floor(Math.random() * tanah.length);
-  const tRandom = tanah[t];
-  if (tRandom == tanahSebelumnya) {
-    randomTanah(tanah);
-  }
-  tanahSebelumnya = tRandom;
-  return tRandom;
+function run(){
+    const i = Math.floor(Math.random() * holes.length)
+    const hole = holes[i]
+    let timer = null
+
+    const img = document.createElement('img')
+    img.classList.add('mole')
+    img.src = 'mole.png'
+
+    img.addEventListener('click', () => {
+        score += 1
+        sound.play()
+        scoreEl.textContent = score
+        img.src = 'mole-whacked.png'
+        clearTimeout(timer)
+        setTimeout(() => {
+            hole.removeChild(img)
+            run()
+        }, 500)
+    })
+
+    hole.appendChild(img)
+
+    timer = setTimeout(() => {
+        hole.removeChild(img)
+        run()
+    }, 1500)
 }
+run()
 
-function randomWaktu(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-function munculkanTikus() {
-  const tRandom = randomTanah(tanah);
-  const wRandom = randomWaktu(600, 800);
-  tRandom.classList.add('muncul');
-
-  setTimeout(() => {
-    tRandom.classList.remove('muncul');
-    if (!selesai) {
-      munculkanTikus();
-    }
-  }, wRandom);
-}
-
-function mulai() {
-  selesai = false;
-  skor = 0;
-  papanSkor.textContent = 0;
-  munculkanTikus();
-  setTimeout(() => {
-    selesai = true;
-  }, 15000);
-}
-
-function pukul() {
-  skor++;
-  this.parentNode.classList.remove('muncul');
-  pop.play();
-  papanSkor.textContent = skor;
-}
-
-tikus.forEach(t => {
-  t.addEventListener('click', pukul);
-});
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + 'px'
+    cursor.style.left = e.pageX + 'px'
+})
+window.addEventListener('mousedown', () => {
+    cursor.classList.add('active')
+})
+window.addEventListener('mouseup', () => {
+    cursor.classList.remove('active')
+})
